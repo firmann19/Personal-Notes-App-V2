88f0b4154f329d89463a3e20717587ae1bcf8537
firmann19 <firman19@gmail.com>
2022-10-14 19:32:29 +0700

    new update

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from "react";
-import { showFormattedDate } from "../utils/index";
 import { getNote } from "../utils/api";
+import { useParams } from "react-router-dom";
 //import { showFormattedDate } from "../utils/api";
 
-function DetailPage({ id }) {
-  const [note, setNote] = useState({});
+const DetailNote = () => {
+  const [note, setNote] = useState("");
+  const { id } = useParams();
 
   useEffect(() => {
-    getNote(id).then(({ data }) => {
-      setNote(data);
-    });
-  }, [id]);
+    const getData = async () => {
+      const response = await getNote(id);
+      setNote(response.data);
+    };
 
-  const { title, createdAt, body } = note;
+    getData();
+  }, []);
 
   return (
-    <div className="note-app__main">
-      <div className="note-app__detail-note">
-        <h1 className="note-app__detail-note_title">{title}</h1>
-        <p className="note-app__detail-note_date">
-          {showFormattedDate(createdAt)}
-        </p>
-        <p className="note-app__detail-note_body">{body}</p>
+    <main>
+      <div className="detail-page">
+        <h1 className="detail-page__title">{note.title}</h1>
+        <p className="detail-page__createdAt">{note.createdAt}</p>
+        <p className="detail-page__body">{note.body}</p>
       </div>
-    </div>
+    </main>
   );
-}
+};
 
-export default DetailPage;
+export default DetailNote;
